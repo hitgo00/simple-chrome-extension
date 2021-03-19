a1e141b7fbc75a451d363d8c5b87dafac056b3f2
hitg00 <hitgo17@gmail.com>
2021-03-19 17:59:32 +0530

    feat: adds info messaging to pwa
    Signed-off-by: hitg00 <hitgo17@gmail.com>

--- background.js
+++ background.js
@@ -1,8 +1,22 @@
-chrome.runtime.onInstalled.addListener(() => {
+// chrome.runtime.onInstalled.addListener(() => {
+// });
+chrome.runtime.onConnectExternal.addListener((port) => {
+  port.onMessage.addListener(console.log);
+  let strInfo, cpuInfo, memInfo;
   setInterval(() => {
-    chrome.system.cpu.getInfo(console.log);
-    chrome.system.memory.getInfo(console.log);
-    chrome.system.storage.getInfo(console.log);
-    // chrome.system.display.getInfo(true, console.log);
-  }, 2100);
+    chrome.system.cpu.getInfo((info) => {
+      cpuInfo = info;
+    });
+    chrome.system.memory.getInfo((info) => {
+      memInfo = info;
+    });
+    chrome.system.storage.getInfo((info) => {
+      strInfo = info;
+    });
+    port.postMessage({
+      strInfo,
+      cpuInfo,
+      memInfo,
+    });
+  }, 1000);
 });
