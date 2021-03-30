da8325a6303d97927848cfbc7616b5b61fe4a130
hitg00 <hitgo17@gmail.com>
2021-03-30 15:50:13 +0530

    chore: moves interval logic to web-app
    Signed-off-by: hitg00 <hitgo17@gmail.com>

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 // chrome.runtime.onInstalled.addListener(() => {
 // });
 chrome.runtime.onConnectExternal.addListener((port) => {
-  port.onMessage.addListener(console.log);
   let strInfo, cpuInfo, memInfo;
-  setInterval(() => {
+  port.onMessage.addListener((message) => {
     chrome.system.cpu.getInfo((info) => {
       cpuInfo = info;
     });
@@ -18,5 +17,5 @@ chrome.runtime.onConnectExternal.addListener((port) => {
       cpuInfo,
       memInfo,
     });
-  }, 1000);
+  });
 });
