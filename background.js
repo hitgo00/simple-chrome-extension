4526f5ee4bf86f5981ecc42e58a15e215a3435e7
hitg00 <hitgo17@gmail.com>
2021-04-13 19:57:18 +0530

    refactor: adds comments and breaks donw code info smaller fucntions
    Signed-off-by: hitg00 <hitgo17@gmail.com>

--- background.js
+++ background.js
@@ -1,19 +1,68 @@
-chrome.runtime.onConnectExternal.addListener((port) => {
-  let strInfo, cpuInfo, memInfo;
-  port.onMessage.addListener((message) => {
+//to fetch cpu stats
+const getCpuInfo = async () => {
+  return new Promise((resolve, reject) => {
     chrome.system.cpu.getInfo((info) => {
-      cpuInfo = info;
+      if (info) {
+        resolve(info);
+      } else {
+        reject("Unable to get cpu information");
+      }
     });
+  });
+};
+
+//to fetch memory information
+const getMemoryInfo = async () => {
+  return new Promise((resolve, reject) => {
     chrome.system.memory.getInfo((info) => {
-      memInfo = info;
+      if (info) {
+        resolve(info);
+      } else {
+        reject("Unable to get memory information");
+      }
     });
+  });
+};
+
+//to fetch storage information
+const getStorageInfo = async () => {
+  return new Promise((resolve, reject) => {
     chrome.system.storage.getInfo((info) => {
-      strInfo = info;
+      if (info) {
+        resolve(info);
+      } else {
+        reject("Unable to get storage information");
+      }
     });
-    port.postMessage({
-      strInfo,
-      cpuInfo,
-      memInfo,
-    });
+  });
+};
+
+const fetchDataFromSystem = () => {
+  return new Promise((resolve, reject) => {
+    Promise.all([getCpuInfo(), getMemoryInfo(), getStorageInfo()]).then(
+      (values) => {
+        const cpuInfo = values[0];
+        const memInfo = values[1];
+        const strInfo = values[2];
+        resolve({
+          strInfo,
+          cpuInfo,
+          memInfo,
+        });
+      }
+    );
+  });
+};
+
+//listen to incoming port connection requests
+chrome.runtime.onConnectExternal.addListener((port) => {
+  port.onMessage.addListener(async (message) => {
+    try {
+      const systemInfo = await fetchDataFromSystem();
+      console.log(systemInfo);
+      port.postMessage(systemInfo);
+    } catch (error) {
+      port.postMessage(error);
+    }
   });
 });
